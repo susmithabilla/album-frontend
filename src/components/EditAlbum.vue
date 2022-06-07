135120ff6e5ab84ad0f22671ffe16a3e7a3d1d9f
susmithabilla <susmitha.billa@eagles.oc.edu>
2022-06-07 01:19:35 -0500

    added menu

--- src/components/EditAlbum.vue
+++ src/components/EditAlbum.vue
@@ -29,7 +29,7 @@
                     <label for="default-input"
                         class="block mb-2 text-left text-sm font-medium text-gray-900 dark:text-gray-300">Released
                         Year</label>
-                    <input type="text" id="default-input" v-model="album.year"
+                    <input type="text" id="default-input" v-model="album.publishedYear"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
                 <div class="mb-6">
@@ -102,6 +102,7 @@ export default {
                 id:this.album.id,
                 name: this.album.name,
                 artist: this.album.artist,
+                publishedYear:this.album.publishedYear,
                 description: this.album.description
             };
 
